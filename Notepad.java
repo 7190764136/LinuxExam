65c6723354093126628352cddbe677d6029bd7ce
7190764136 <1975009729@qq.com>
2021-12-29 00:59:38 +0800

    Notepad.java

---
+++ Notepad.java
@@ -0,0 +1,12 @@
+ckage com.example;
+
+public class Notepad {
+    int id;
+    String notepadContent;
+    String notepadTime;
+
+    @Override
+    public String toString() {
+        return "Notepad [id=" + id + ", notepadContent=" + notepadContent + ", notepadTime=" + notepadTime + "]";
+    }
+}
